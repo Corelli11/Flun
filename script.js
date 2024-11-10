7c7b7de74638db9f5fbb4c9f6a00a9c10dff9298
Corelli11 <143276194+Corelli11@users.noreply.github.com>
2024-11-10 14:13:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+// script.js
+document.getElementById('changeColorBtn').addEventListener('click', function() {
+    document.body.style.backgroundColor = document.body.style.backgroundColor === 'lightblue' ? '#f4f4f4' : 'lightblue';
+});
 
